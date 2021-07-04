e4b2b239cee04d6dd9372a035d4221f7f4694ec6
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-04 13:02:34 +0900

    첫수업 커밋

---
+++ Ex1/Ex1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "숫자를 입력하세요.\n" << std::endl;
+
+    int num;
+
+    std::cin >> num;
+
+    std::cout << "입력하신 숫자는";
+    std::cout << num;
+    std::cout << "입니다";
+}
