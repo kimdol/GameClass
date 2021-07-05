1bfdb7ff4de4d3aa9e384cf01aa201c81d2d8949
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-05 17:02:43 +0900

    64비트, 32비트의 사이즈 측정(int, long)

--- Ex1_2/Ex1_2.cpp
+++ Ex1_2/Ex1_2.cpp
@@ -15,6 +15,9 @@ int main()
     char myChar;
     myChar = 'A';
 
-    std::cout << myChar;
+    std::cout << myChar << std::endl;
+
+    std::cout << sizeof(int) << std::endl;
+    std::cout << sizeof(long) << std::endl;
 
 }
