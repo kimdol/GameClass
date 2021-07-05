840f9f42b20d2a94c76cb1901c470b82e1ba6d7d
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-05 15:59:44 +0900

    선언과 대입 그리고 l-value와 r-value를 설명함 추가로 여 챕터의 날짜에 해당하는 영상에 2진수, 16진수 그리고 2의 보수도 설명함

---
+++ Ex1_2/Ex1_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    // 선언 (declaration)
+    int myInteger;  // 이름 + 형 이여야한다.
+
+    // 대입 (assignment)
+    myInteger = 1;
+
+    int x;
+
+    x = myInteger;  // l-value, r-value
+
+}
