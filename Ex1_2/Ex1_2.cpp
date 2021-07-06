bf5eb164cb1ccfb2649da7edc0547f6ec2eda3b4
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-06 16:35:50 +0900

    오버플로우, 언더플로우

--- Ex1_2/Ex1_2.cpp
+++ Ex1_2/Ex1_2.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    /*
     // 선언 (declaration)
     int myInteger;  // 이름 + 형 이여야한다.
 
@@ -19,5 +20,26 @@ int main()
 
     std::cout << sizeof(int) << std::endl;
     std::cout << sizeof(long) << std::endl;
+    */
+    /*
+    char input_c;
+    float input_f;
+    bool input_b;
+
+    std::cout << "문자를 입력하시오  ";
+    std::cin >> input_c;
+    std::cout << "입력하신 문자 : " << input_c << std::endl;
+
+
+    std::cout << "소수를 입력하시오  ";
+    std::cin >> input_f;
+    std::cout << "입력하신 소수 : " << input_f << std::endl;
+
+    std::cout << "bool를 입력하시오  ";
+    std::cin >> input_b;
+    std::cout << "입력하신 bool : " << input_b << std::endl;
+    */
+    int myNum = 2147483647;
+    std::cout << myNum + 1; // 보수의 이유로 -가 나옴
 
 }
