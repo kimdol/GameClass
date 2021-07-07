0934f233c86e2785ba9db6464b42fdc8ff050108
GameCanvas 2RoomSub <gamecanvas01@gmail.com>
2021-07-07 16:54:07 +0900

    비트 연산

---
+++ Ex_1_3_2/Ex_1_3_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	//int number1{ 3 }, number2{ 2 };
+
+	//std::cout << number1 + number2 << std::endl;
+	//std::cout << number1 % number2 << std::endl;
+
+	int number = 0b0010;
+
+	//number = number >> 1; // 0001로 밀림
+	number = number << 2;
+
+	std::cout << number << std::endl;
+
+}
+
