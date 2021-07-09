35ca446769ef20cbbcab44eba103ebb5aaab3603
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-09 17:05:28 +0900

    swap과 연산자 활용

---
+++ Ex1_3_3/Ex1_3_3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+    int x, y, tmp;
+
+    std::cout << "x와 y를 입력하시오. (space바 이용)" << std::endl;
+    std::cin >> x >> y;
+
+    std::cout << "몫 : " << x / y <<
+        "\n나머지 : " << x % y << std::endl;
+
+
+
+    std::cout << "x와 y를 또 입력하시오. (space바 이용)" << std::endl;
+    std::cin >> x >> y;
+
+    x = (x << 8) | y;
+
+    y = (x >> 8) & 0xFF;
+    x = x & 0xFF;
+    
+    std::cout << "x : " << x <<
+        "\ny : " << y << std::endl;
+
+
+
+
+    std::cout << "x와 y를 또또 입력하시오. (space바 이용)" << std::endl;
+    std::cin >> x >> y;
+
+    tmp = x;
+    x = y;
+    y = tmp;
+
+    std::cout << "x : " << x <<
+        "\ny : " << y << std::endl;
+
+
+}
--- Ex_1_3_2/Ex_1_3_2.cpp
+++ Ex_1_3_2/Ex_1_3_2.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	/*
 	//int number1{ 3 }, number2{ 2 };
 
 	//std::cout << number1 + number2 << std::endl;
@@ -19,10 +20,40 @@ int main()
 
 	number = number << 1;
 	std::cout << number << std::endl;
+	*/
 
 	// 여기서 알수있는 부분은 곱하기 2가 된다는 것이다.
 	// 이러한 비트연산자는 RGB에 씀
 	// RGB + 알파(투명도) = 4byte
+
+	int alpha { 255 };
+	int red { 128 };
+	int green{ 64 };
+	int blue{ 32 };
+
+	unsigned int color{ 0 };
+
+	// color <= argb
+	color = color | (alpha << 24);
+
+	color = color | (red << 16);
+
+	color = color | (green << 8);
+
+	color = color | blue;
+
+	std::cout << color << std::endl;
+
+	// color => argb
+	blue = color & 0xFF;
+	green = (color >> 8) & 0xFF;
+	red = (color >> 16) & 0xFF;
+	alpha = (color >> 24) & 0xFF;
+
+	std::cout << alpha << std::endl;
+	std::cout << red << std::endl;
+	std::cout << green << std::endl;
+	std::cout << blue << std::endl;
 
 
 }
