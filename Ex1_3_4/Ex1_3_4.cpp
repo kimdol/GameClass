ad971eb650ca15ba9b39679f642f97892f97bbde
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-12 16:08:37 +0900

    환율 환산 접미어 활용

--- Ex1_3_4/Ex1_3_4.cpp
+++ Ex1_3_4/Ex1_3_4.cpp
@@ -14,9 +14,11 @@ int main()
     std::cout << "절대값 : " << absoluteNum << std::endl;*/
 
     int inputNum;
-    float usdMoney, krwMoney;
+    float money, krwMoney;
+    char kind;
     std::string evenOdd;
-    const float EXCHANGERATE{ 1146.57f };   // 유니폼 초기화 할 때, 리터럴을 꼬옥! 써줘야한다.
+    const float USDTOWON{ 1146.57f };   // 유니폼 초기화 할 때, 리터럴을 꼬옥! 써줘야한다.
+    const float JPYTOWON{ 10.41f };
 
     std::cout << "홀수 짝수 구별 할 숫자를 입력하시오. ";
     std::cin >> inputNum;
@@ -25,11 +27,20 @@ int main()
     std::cout << "결과는 " << evenOdd << "입니다." << std::endl;
 
 
-    std::cout << "KRW로 바꿀 USD를 입력하시오. ";
-    std::cin >> usdMoney;
+    std::cout << "달러(S) 혹은 엔(Y)를 입력하시오. ";
+    std::cin >> money >> kind;
 
-    krwMoney = usdMoney * EXCHANGERATE;
-    std::cout << "결과는 " << krwMoney << "원 입니다." << std::endl;
+    if (kind == 'S')
+    {
+        krwMoney = money * USDTOWON;
+        std::cout << "결과는 " << krwMoney << "원 입니다." << std::endl;
+    }
+    else if (kind == 'Y')
+    {
+        krwMoney = money * JPYTOWON;
+        std::cout << "결과는 " << krwMoney << "원 입니다." << std::endl;
+    }
+    
 
 
 
