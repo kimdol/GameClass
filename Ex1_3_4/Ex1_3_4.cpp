87c0932cb1c62d238d07707756f6b2a1142ff0be
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-12 15:25:16 +0900

    절대값 계산

---
+++ Ex1_3_4/Ex1_3_4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int inputNum;
+
+    std::cout << "숫자를 입력하시오. ";
+    std::cin >> inputNum;
+    
+    int absoluteNum;
+
+    absoluteNum = inputNum < 0 ? -inputNum : inputNum;
+
+    std::cout << "절대값 : " << absoluteNum << std::endl;
+
+
+
+}
