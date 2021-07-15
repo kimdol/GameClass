67a01360c575fb5fd6045e57053de0ae4a286ee4
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-15 16:58:29 +0900

    구조체 등등 타입공부(플라그마 개념도 추가)

---
+++ Ex3_5/Ex3_5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	//int myInt{ 1000 };
+	//unsigned char myChar = myInt;
+
+	//std::cout << myInt << "\t" << (int)myChar << std::endl; // 위에서 이미 잘림
+
+	struct OneByte
+	{
+		char myChar;
+	};
+
+	std::cout << sizeof(OneByte) << std::endl;
+
+#pragma pack(push, 1)	// 1바이트로 바꾸기
+	struct FiveByte
+	{
+		char myChar;
+		int myInt;
+	};
+#pragma pack(pop)
+
+	std::cout << sizeof(FiveByte) << std::endl;
+}
