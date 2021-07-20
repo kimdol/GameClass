263e9a0229d78e8d349e5178c944cf7ccf6e13a0
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-20 19:15:05 +0900

    배열마스터와 문자열기초

--- Ex3_5/Ex3_5.cpp
+++ Ex3_5/Ex3_5.cpp
@@ -7,24 +7,24 @@ int main()
 
 	//std::cout << myInt << "\t" << (int)myChar << std::endl; // 위에서 이미 잘림
 
-//	{
-//		struct OneByte
-//		{
-//			char myChar;
-//		};
-//
-//		std::cout << sizeof(OneByte) << std::endl;
-//
-//#pragma pack(push, 1)	// 1바이트로 바꾸기
-//		struct FiveByte
-//		{
-//			char myChar;
-//			int myInt;
-//		};
-//#pragma pack(pop)
-//
-//		std::cout << sizeof(FiveByte) << std::endl;
-//	}
+	//	{
+	//		struct OneByte
+	//		{
+	//			char myChar;
+	//		};
+	//
+	//		std::cout << sizeof(OneByte) << std::endl;
+	//
+	//#pragma pack(push, 1)	// 1바이트로 바꾸기
+	//		struct FiveByte
+	//		{
+	//			char myChar;
+	//			int myInt;
+	//		};
+	//#pragma pack(pop)
+	//
+	//		std::cout << sizeof(FiveByte) << std::endl;
+	//	}
 	/*{
 		union MyUnion
 		{
@@ -85,8 +85,8 @@ int main()
 	/*배열 연습문제 1-2*/
 	/*{
 		const int CLASSNUM{ 5 };
-		int reportCard[CLASSNUM]{}, rank[CLASSNUM]{}, tmp[CLASSNUM]{};
-		int ranking{ 1 }, top[2]{ 0, 0 };
+		int reportCard[CLASSNUM]{}, ranking[CLASSNUM]{}, scoreTmp[CLASSNUM]{};
+		int rank{ 1 }, top[2]{ 0, 0 };
 
 		std::cout << "학생 점수를 입력하라 \n";
 		std::cout << "입력 : ";
@@ -94,45 +94,97 @@ int main()
 		for (int i = 0; i < CLASSNUM; i++)
 		{
 			std::cin >> reportCard[i];
-			tmp[i] = reportCard[i];
+			scoreTmp[i] = reportCard[i];
 		}
 
 		for (int i = 0; i < CLASSNUM; i++)
 		{
 			for (int k = 0; k < CLASSNUM; k++)
 			{
-				if (top[0] < tmp[k])
+				if (top[0] < scoreTmp[k])
 				{
-					top[0] = tmp[k];
+					top[0] = scoreTmp[k];
 					top[1] = k;
 				}
 			}
-			tmp[top[1]] = 0;
-			rank[top[1]] = ranking++;
+			top[0] = 0;
+			scoreTmp[top[1]] = 0;
+			ranking[top[1]] = rank++;
 		}
-		
+
 		for (int i = 0; i < CLASSNUM; i++)
 		{
-			std::cout << reportCard[i] << " ";
+			std::cout << reportCard[i] << "\t";
 		}
 		std::cout << "\n";
-		
+		for (int i = 0; i < CLASSNUM; i++)
+		{
+			std::cout << ranking[i] << "\t";
+		}
+
 
 	}*/
-	/* 문자열 공부 */
+	/*배열 연습문제 1-3*/
 	{
-		char array[6]{ 'd', 'o', 'g', 'g', 'y' };
+		const int CLASSNUM{ 5 };
+		int reportCard[CLASSNUM]{};
+		int scoreTmp{ 0 };
+		
+		std::cout << "학생 점수를 입력하라 \n";
+		std::cout << "입력 : ";
 
-		//array[5] = '\0';
-		std::cout << array << std::endl;
+		for (int i = 0; i < CLASSNUM; i++)
+		{
+			std::cin >> reportCard[i];
+		}
 
-		char myStr[10];
-		std::cout << "Input String : ";
-		// 넘치게 입력하면 오버플로우가 일어나서 stack(메모리에 올라가는 그 스택)이 손상됨
-		std::cin >> myStr;
-		std::cout << "your string is \"" << myStr << "\"" <<
-			std::endl;
+		for (int i = (CLASSNUM - 1); i > 0; i--)
+		{
+			for (int k = 0; k < i; k++)
+			{
+				if (reportCard[k] > reportCard[k+1])
+				{
+					scoreTmp = reportCard[k + 1];
+					reportCard[k + 1] = reportCard[k];
+					reportCard[k] = scoreTmp;
+				}
+			}
+		}
 
+		for (int i = 0; i < CLASSNUM; i++)
+		{
+			std::cout << reportCard[i] << "\t";
+		}
 
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	///* 문자열 공부 */
+	//{
+	//	char array[6]{ 'd', 'o', 'g', 'g', 'y' };
+
+	//	//array[5] = '\0';
+	//	std::cout << array << std::endl;
+
+	//	char myStr[10];
+	//	std::cout << "Input String : ";
+	//	// 넘치게 입력하면 오버플로우가 일어나서 stack(메모리에 올라가는 그 스택)이 손상됨
+	//	std::cin >> myStr;
+	//	std::cout << "your string is \"" << myStr << "\"" <<
+	//		std::endl;
+
+
+	//}
 }
