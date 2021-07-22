3e6ab3b6fc3fb0e1f2f32b2ef8f9d751af8db3c9
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-22 19:18:14 +0900

    테트릭스 게임

--- Ex4_1/Ex4_1.cpp
+++ Ex4_1/Ex4_1.cpp
@@ -22,32 +22,66 @@ int main()
         {
             if ((flag & 0x0001) == 0x0000)
             {
-                for (int row = 0; row)
-                std::cout << "왼쪽(1)과 오른쪽(2)을 누르세요. ";
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (tetris.a[row][col] == 1)
+                        {
+                            std::cout << "O ";
+                        }
+                        else
+                        {
+                            std::cout << "  ";
+                        }
+                    }
+                    std::cout << "\n";
+                }
+                std::cout << "왼쪽(1)과 오른쪽(2)과 끝내기(3)을 누르세요. ";
                 std::cin >> command;
-                flag = 0x0001;
-            }
-            else if ((flag & 0x0001) == 0x0001)
-            {
-                for (int row = 0; row)
-                if (command == 1)   // 왼쪽 누름
+                if (command == 3)
                 {
-                    rotBlock[2-col][row] = tetris[row][col];
+                    flag = 0x1001;
                 }
-                else if (command == 2) // 오른쪽 누름
+                else
                 {
-                    rotBlock[col][2-row] = tetris[row][col];
-                }
+                    flag = 0x0011;
+                }      
             }
-            else if ()      // 
+            else if ((flag & 0x0010) == 0x0010)     // 회전하고 싶을 때
             {
                 for (int row = 0; row < 3; row++)
                 {
                     for (int col = 0; col < 3; col++)
                     {
-                        tetris[row][col] = rotBlock[row][col];
+                        if (command == 1)   // 왼쪽 누름
+                        {
+                            rotBlock[2 - col][row] = tetris.a[row][col];
+                        }
+                        else if (command == 2) // 오른쪽 누름
+                        {
+                            rotBlock[col][2 - row] = tetris.a[row][col];
+                        }
                     }
                 }
+                flag = 0x0101;
+            }
+            else if ((flag & 0x0100) == 0x0100)      // 회전처리 다했을 때
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    
+                    for (int col = 0; col < 3; col++)
+                    {
+                        tetris.a[row][col] = rotBlock[row][col];
+                    }
+                }
+                flag = 0x0000;
+            }
+            else if ((flag & 0x1000) == 0x1000)      // 테트릭스 게임을 끝내고 싶을 때
+            {
+                std::cout << "테트릭스 게임이 종료되었습니다. " << std::endl;
+                break;
             }
         }
     }
