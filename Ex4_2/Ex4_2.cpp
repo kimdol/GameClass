1356eed6a3e428f3cec85835f1507f7e3cc5eefc
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-22 22:00:38 +0900

    중간점검 연습문제 1-1

--- Ex4_2/Ex4_2.cpp
+++ Ex4_2/Ex4_2.cpp
@@ -4,6 +4,36 @@ int main()
 {
     /*중간점검 연습문제 1-1*/
     {
+        const int TMPNUM{ 24 };
+        int  pascal[TMPNUM][TMPNUM]{};
+        int m{ 1 }, n{ 1 };
 
+        pascal[1][TMPNUM / 2] = 1;
+
+        while (m < 6)
+        {
+            for (n = 1; n < TMPNUM-1; n++)
+            {
+                if (!((m == 1) && (n == TMPNUM / 2)))
+                {
+                    pascal[m][n] = pascal[m - 1][n - 1] + pascal[m - 1][(n - 1) + 2];
+                }
+                else
+                {}
+
+                if (0 < pascal[m][n])
+                {
+                    std::cout << pascal[m][n];
+                }
+                else if (pascal[m][n] == 0)
+                {
+                    std::cout << " ";
+                }
+            } 
+            std::cout << "\n";
+            m++;
+        }
+
+       
     }
 }
