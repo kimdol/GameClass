b1494f30558417e3deb298052bb1150239c2eb33
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-07-27 16:53:21 +0900

    매개변수 배열로 받기

--- Ex5_2/Ex5_2.cpp
+++ Ex5_2/Ex5_2.cpp
@@ -1,11 +1,28 @@
 ﻿#include <iostream>
+int Sum(int num[5])
+{
+    int total{ 0 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        total += num[i];
+    }
+    return total;
+}
+
 
 int main()
 {
     /* 함수 기본 */
-    {
+    /*{
         std::cout << pow(2, 2) << std::endl;
 
+    }*/
+    /*함수에 매개변수로 배열을 사용해 봅시다*/
+    {
+        int num[]{ 1, 2, 3, 4, 5 };
+
+        std::cout << Sum(num) << std::endl;
     }
     return 0;
 }
