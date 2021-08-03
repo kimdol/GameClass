b33ade54289f3bca8060998def12302bf9a8285c
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-08-03 17:11:50 +0900

    포인터 기초 개념1

---
+++ Ex6_1/Ex6_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    /*포인터 기초*/
+    /*int a{ 0 }, b{ 0 };
+    int* p;
+
+    p = &a;
+    p = &b;*/
+    /*포인터 기초2*/
+    /*int a{ 1 };
+    int* p{ &a };
+
+    std::cout << p << std::endl;
+    std::cout << p + 1 << std::endl;
+    std::cout << p + 2 << std::endl;*/
+    /*포인터 기초3*/
+    /*int a{ 1025 };
+    int* pa{ &a };
+
+    char* p0{ (char*)pa };
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << (void*)p0 << " : " << (int)*p0 << std::endl;
+
+        p0++;
+    }*/
+}
