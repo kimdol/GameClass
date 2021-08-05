06ce6563e481df01161f9407efc014002bdab435
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-08-05 17:10:21 +0900

    함수포인터

--- Ex6_1/Ex6_1.cpp
+++ Ex6_1/Ex6_1.cpp
@@ -7,6 +7,36 @@ void Swap(int* a, int* b)
     *b = tmp;
 }
 
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Fun(float f)
+{
+    return 0;
+}
+
+using funcion_ptr = int(*)(float);
+// 정렬
+void Sort(int numbers[], int count)
+{
+    int tmp{};
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = i + 1; j < count; j++)
+        {
+            if (numbers[i] < numbers[j]) // 이부분을 함수로 만들어서 내림차순, 오름차순을 변경한다
+            {
+                tmp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = tmp;
+            }
+        }
+    }
+}
+
 int main()
 {
     /*포인터 기초*/
@@ -35,11 +65,39 @@ int main()
         p0++;
     }*/
     /*포인터 기초4*/
-    
-    int x{ 10 }, y{ 20 };
+    /*int x{ 10 }, y{ 20 };
     
     Swap(&x, &y);
 
-    std::cout << x << ", " << y << std::endl;
+    std::cout << x << ", " << y << std::endl;*/
+    /*포인터 기초5*/
+    /*int a{}, b{}, arr[3]{};
 
+    std::cout << &a << " ," << &b << std::endl;
+    std::cout << &Sum << " ," << &main << std::endl;
+    std::cout << Sum << " ," << main << std::endl;
+
+    std::cout << arr << " ," << &arr << std::endl;*/
+    /*함수 포인터 기초 1*/
+    /*const int NumArray = 5;
+    int scores[NumArray]{ 20, 10, 40, 15, 30 };
+
+    Sort(scores, NumArray);
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << scores[i] << ", ";
+    }
+    std::cout << std::endl;*/
+    /*배열과 포인터 관계*/
+    int arr[5]{};
+    int* p;
+
+    p = arr;
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << &arr[i] << std::endl;
+    }
+
+    std::cout << arr << " ," << p << std::endl;
 }
