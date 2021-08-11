16d0e2fac6b014710311b435e24c80d6e81e75cb
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-08-11 17:44:37 +0900

    2차원 배열과 함수 기초와 문자열 한글 처리

--- Ex6_2/Ex6_2.cpp
+++ Ex6_2/Ex6_2.cpp
@@ -25,6 +25,20 @@ int Sum2(int (*input)[3], int a, int b)
 
     return sum;
 }
+int Sum3(int input[][3], int a, int b)
+{
+    int sum{};
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            sum += input[i][j];
+        }
+    }
+
+    return sum;
+}
 
 int main()
 {
@@ -35,14 +49,16 @@ int main()
     };
 
     std::cout << Sum(&numbers[0][0], 6) << std::endl;
-    std::cout << Sum2(numbers, 2, 3) << std::endl;*/
+    std::cout << Sum2(numbers, 2, 3) << std::endl;
+    std::cout << Sum3(numbers, 2, 3) << std::endl;*/
     /*문자열 한글 처리*/
-    wchar_t myStr[] = { L" 는 정수입니다." }; // L이 꼭 있어야함
+    // L이 꼭 있어야함
+    /*wchar_t myStr[] = { L" 는 정수입니다." };
 
     std::locale myLoc("kor");
     std::wcout.imbue(myLoc);
 
     std::wcout << myStr << std::endl;
-    std::wcout << myStr + 5 << std::endl;
+    std::wcout << myStr + 5 << std::endl;*/
 
 }
