d32141d5ea6d0860b46b7fd52947f2b6763a326b
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-08-12 16:51:51 +0900

    문자열 처리(포인터)와 C++스타일 동적 할당

--- Ex6_2/Ex6_2.cpp
+++ Ex6_2/Ex6_2.cpp
@@ -39,6 +39,17 @@ int Sum3(int input[][3], int a, int b)
 
     return sum;
 }
+int GetLength(char str[])
+{
+    int cnt{ 0 };
+    while (1)
+    {
+        if (*(str + cnt++) == '\0')
+        {
+            return cnt-1;
+        }
+    }
+}
 
 int main()
 {
@@ -60,5 +71,45 @@ int main()
 
     std::wcout << myStr << std::endl;
     std::wcout << myStr + 5 << std::endl;*/
+    /* 문자열의 길이 */
+    /*char input[1000];
+    std::cout << "문자열을 입력하라 ";
+    std::cin >> input;
+
+    std::cout << GetLength(input) << std::endl;*/
+    /* 문자열 비교 */
+    // ctrcmp 는 길이나 사이즈를 비교하는것이 아닌
+    // 아스키코드를 비교한다.
+    /* 메모리 동적 할당(차이점)*/
+    //  현업에서도 아직까지 살아있을 수 도있음
+    /*int array[100];
+
+    int* buffer1, * buffer2;
+
+    buffer1 = (int*)calloc(100, sizeof(int));
+    // 게임 프로그래머 선호 - 속도(초기화를 안하기 때문)
+    buffer2 = (int*)malloc(100 * sizeof(int));
+
+    if (buffer1 == nullptr || buffer2 == nullptr)
+    {
+        return 0;
+    }
+
+    std::cout << buffer1[0] << ",  " << buffer2[0] <<
+        std::endl;
+
+    free(buffer1);
+    free(buffer2);
+
+    return 0;*/ 
+    /*C++ 스타일  동적  할당*/
+    int* p1{new int {4}};
+    // int *p1;
+    // p1 = new int;
+    // *p1 = 4;
+    // 와 같다.
+
+    delete p1;
+
 
 }
