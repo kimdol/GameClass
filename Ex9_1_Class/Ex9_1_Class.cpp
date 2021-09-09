7dff2ca726987e5623d96c16fe7ca83cc9bf3db3
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-09-09 16:52:12 +0900

    class 첫 시작

---
+++ Ex9_1_Class/Ex9_1_Class.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Dog
+{
+private:
+    int mAge;
+
+public:
+    int mWeight;
+
+    void Function()
+    {
+        mAge = 1;
+    }
+
+};
+
+int main()
+{
+    Dog happy;
+
+    std::cout << sizeof(Dog) << std::endl;
+
+}
