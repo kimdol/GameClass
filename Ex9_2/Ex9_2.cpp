98f2c8d24bdc14d787b6294d421041666335f937
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-09-17 16:53:55 +0900

    대리생성자 활용

---
+++ Ex9_2/Ex9_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+// 대리생성자 활용
+class MyClass
+{
+public:
+    int mVal1;
+    int mVal2;
+
+    MyClass() : MyClass(1, 1)
+    {
+
+    }
+
+    MyClass(int v1, int v2) : mVal1{ v1 }, mVal2{ v2 }
+    {
+
+    }
+};
+
+int main()
+{
+    
+}
