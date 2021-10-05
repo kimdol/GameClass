39c230e63726db9cb2c4be9c1db17a0ac075474b
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-05 17:50:16 +0900

    멤버함수포인터

---
+++ Ex11_1/Ex11_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+class MyFunctions
+{
+public:
+    static int NormalFunction(int x)
+    {
+        return x;
+    }
+
+    static int SquareFunction(int x)
+    {
+        return x * x;
+    }
+};
+using FuncPtr = int (*)(int);
+using FunctionPtr = int (MyFunctions::*)(int);
+
+int Sigma(FuncPtr f, int m, int n)
+{
+    int sum{};
+    for (int k = m; k <= n; k++)
+    {
+        sum += (*f)(k);
+        // sum += (instance.*f)(k);
+    }
+
+    return sum;
+}
+
+int main()
+{
+    std::cout <<
+        Sigma(&MyFunctions::NormalFunction, 1, 10)
+        << std::endl;
+    std::cout <<
+        Sigma(&MyFunctions::SquareFunction, 1, 10)
+        << std::endl;
+}
