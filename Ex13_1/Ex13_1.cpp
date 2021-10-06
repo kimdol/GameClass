1bc7268b5bd9f5c6876d2fbfdda1aab35cc0e72d
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-06 16:49:50 +0900

    STL

---
+++ Ex13_1/Ex13_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    // STL 사용법1
+    /*std::string s1, s2;
+
+    s1 = s2;
+    s1 += s2;
+    s1[0] = 'a';
+    s1 = s1 + s2;
+    s1 == s2;
+
+    s1.c_str(); // char *(c스타일로 get함)
+    s1.insert(pos, s2);
+    s1.appedn(s2);*/
+
+}
