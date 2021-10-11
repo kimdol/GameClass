b8b3165a3ba0d2c1a6d4a9ef838f67005802a2b4
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-11 15:07:18 +0900

    stl 연습장

---
+++ stl_ex1/vector.cpp
@@ -0,0 +1,88 @@
+    // STL(벡터) 기초
+    /*  std::vector<int> v{2, 3, 1, 5, 6};
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << std::endl;
+    }
+    // range-based loop (범위 기반 반복문)
+    for (int x : v) // -> [ 0 : size() )
+    {
+        std::cout << x << std::endl;
+    } */
+    // STL(벡터) 연습1
+    /*std::vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    v.pop_back();
+
+    for (int e : v)
+    {
+        std::cout << e << std::endl;
+    }*/
+    // STL(벡터) 연습2
+    /*std::vector<int> v;
+    int value, sum{ 0 };
+
+    while (std::cin >> value)
+    {
+        v.push_back(value);
+    }
+
+    for (int e : v)
+    {
+        sum += e;
+    }
+
+    std::cout << "합 : "
+        << sum << "\n"
+        << "평균 : "
+        << static_cast<float>(sum) / v.size()
+        << std::endl;*/
+    // STL(벡터) 연습3
+    /*  std::vector<int>                v{1, 2, 3};
+            std::vector<int>::iterator      itr = v.begin();
+            auto itr2 = v.begin(); // 반환값 타입을 보고 auto를 추측!
+
+            while (itr != v.end())
+            {
+                std::cout << *itr;
+                itr++;
+            }
+            */
+    // 연습 4
+    /*std::vector<int>    v{ 1, 2, 3 };
+
+            for (auto& e : v)
+            {
+                e = 0;
+            }
+
+            for (int e : v)
+            {
+                std::cout << e << std::endl;
+            }*/
+    // STL 알고리즘 연습1
+    /*  std::vector<int> v1{2, 1, 3};
+
+            // qsort(v.data(), v.size(), sizeof(int), &IntCompare);
+            qsort(v1.data(), v1.size(), sizeof(int),
+                [](const void* a, const void* b)
+                {
+                    return *((int*)b) - *((int*)a);
+                }
+            );
+
+            std::for_each(v1.begin(), v1.end(), [](int val)
+                {
+                    std::cout << val << std::endl;
+                }
+            );
+
+            for (auto e : v1)
+            {
+                std::cout << e << std::endl;
+            } */
