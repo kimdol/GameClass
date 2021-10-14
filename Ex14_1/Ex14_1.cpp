b5720dc92ff998237b41b92ea5131c7df4682bed
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-14 18:23:16 +0900

    모던 c++ 중간정도함

---
+++ Ex14_1/Ex14_1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    // 람다식
+    {
+        // 매개변수를 2개 int를 받는 익명함수
+        auto expr1 = [](int a, int b) {};
+        // 매개변수 int 1개와 반환값 float을 돌려주는 익명함수
+        [](int a) -> float { return 0.0f; };
+    }
+}
