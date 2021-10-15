08c29a9deaa0bdc5e9590a3ac485d3ae0307c38a
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-15 16:53:51 +0900

    모던c++_중간2

--- Ex14_1/Ex14_1.cpp
+++ Ex14_1/Ex14_1.cpp
@@ -1,5 +1,12 @@
 ﻿#include <iostream>
 
+// 구조체 메모리 지정
+//struct alignas(1) MyStruct
+//{
+//    char val2;
+//    int val1;
+//};
+
 int main()
 {
     // 람다식
@@ -8,5 +15,10 @@ int main()
         auto expr1 = [](int a, int b) {};
         // 매개변수 int 1개와 반환값 float을 돌려주는 익명함수
         [](int a) -> float { return 0.0f; };
+    }
+    // 화면에 "'\'" 출력하라(유용한 방법은 로우 리터럴)
+    {
+        std::cout << "\"\'\\\'\"" << std::endl;
+        std::cout << R"( s "'\'" s )" << std::endl;
     }
 }
