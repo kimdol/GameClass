1f2862b2f6383a06e82ca3cbe7ff130f0f7e8867
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-20 17:00:39 +0900

    파일 스트림

---
+++ Ex15_1/Ex15_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+
+bool LoadFile(const char* filename)
+{
+    // 1. 파일 스트림 객체(입력)
+    std::ifstream ifs(filename);
+    if (!ifs)
+    {
+        std::cout << filename << "을 찾을 수 없습네다" << std::endl;
+        return false;
+    }
+    // 2. 작업 (데이터 꺼내오기)
+    char ch;
+    while (ifs >> ch)
+    {
+        std::cout << ch;
+    }
+    // 3. 객체 제거(파일 닫기)
+    ifs.close();
+    return true;
+}
+
+int main()
+{
+    // 파일 스트림
+    {
+        LoadFile("F:\\DirectX_Class\\GameClass\\Ex15_1\\Data\\SimpleData.txt");
+        
+        
+    }
+}
