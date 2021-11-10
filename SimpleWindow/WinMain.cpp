2c973f7f358b566cd9eb715bc48f60e879662884
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-11-10 17:54:24 +0900

    GDIPlus 완성

--- SimpleWindow/WinMain.cpp
+++ SimpleWindow/WinMain.cpp
@@ -2,6 +2,10 @@
 #include <Windows.h>
 // 문자열
 #include <sstream>
+// GDI+
+#include <gdiplus.h>
+#pragma comment (lib, "Gdiplus.lib")
+// using namespace Gdiplus;
 
 const wchar_t gClassName[] = L"MyWindowClass";
 
@@ -17,6 +21,10 @@ int WINAPI WinMain(
 {
 	/*첫 시작*/
 	// MessageBox(nullptr, L"HelloWorld", L"SimpleWindow", MB_ICONEXCLAMATION | MB_OK);
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR token;
+
+	Gdiplus::GdiplusStartup(&token, &gdiplusStartupInput, nullptr);
 	
 	/* 윈도우를 만들기 위한 4단계 */
 	// 1. 윈도우 클래스 등록 - 윈도우 프로시져 등록
@@ -47,13 +55,16 @@ int WINAPI WinMain(
 	}
 
 	// 2. 윈도우 생성
+	RECT wr = { 0, 0, 640, 480 };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, false); // 클라이언트 크기 계산
+
 	HWND hwnd = CreateWindowEx(
 		0,
 		gClassName,
 		L"HelloWindow",
 		WS_OVERLAPPEDWINDOW,
-		0, 0,
-		640, 480,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		wr.right - wr.left, wr.bottom - wr.top,
 		0,
 		0,
 		hInstance,
@@ -77,16 +88,84 @@ int WINAPI WinMain(
 		DispatchMessage(&msg);
 	}
 
-
+	Gdiplus::GdiplusShutdown(token);
 	return static_cast<int>(msg.wParam);
 }
 
-// * 윈도우 프로시져 함수 만들기 - callback
+void Example_DrawRectangle2(HWND hwnd)
+{
+	PAINTSTRUCT ps;
 
+	HDC hdc = BeginPaint(hwnd, &ps);
+	//Graphics graphics(hdc);
+
+	HPEN bluePen = CreatePen(PS_DOT, 1, RGB(0, 0, 255));
+	HPEN oldPen = (HPEN)SelectObject(hdc, bluePen);
+
+	// Create a RectF object.
+	//RectF rect(199.0f, 100.0f, 200.0f, 200.0f);
+
+	// Draw rect.
+	//graphics.DrawRectangle(&blackPen, rect);
+}
+
+void OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps;
+
+	HDC hdc = BeginPaint(hwnd, &ps);
+	
+	/*GDI*/
+	/*{
+		HPEN bluePen = CreatePen(PS_DOT, 1, RGB(0, 0, 255));
+		HPEN oldPen = (HPEN)SelectObject(hdc, bluePen);
+
+		HBRUSH hatchBrush = CreateHatchBrush(HS_CROSS, RGB(255, 0, 0));
+		HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, hatchBrush);
+
+		Rectangle(hdc, 0, 0, 100, 100);
+
+		DeleteObject(hatchBrush);
+		SelectObject(hdc, oldBrush);
+
+		DeleteObject(bluePen);
+		SelectObject(hdc, oldPen);
+	}*/
+	/*GDI plus*/
+	Gdiplus::Graphics graphics(hdc);					// DC -> Graphics
+	Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 255));	// HPEN -> Pen
+
+	graphics.DrawRectangle(&pen, 0, 0, 100, 100);
+
+	pen.SetColor(Gdiplus::Color::HotPink);
+	graphics.DrawLine(&pen, 0, 0, 100, 100);
+
+	Gdiplus::SolidBrush brush(Gdiplus::Color::Black);
+	Gdiplus::FontFamily family(L"맑은 고딕");
+	Gdiplus::Font font(&family, 24, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
+	graphics.DrawString(L"맑은 고딕 문자열입니다.", -1, &font, { 0.0f, 110.0f }, &brush);
+	
+	Gdiplus::Image image(L"1.png");
+	/*graphics.DrawImage(&image, Gdiplus::RectF(
+		ps.rcPaint.left, ps.rcPaint.top,
+		ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top
+	));*/
+	graphics.DrawImage(&image, 0, 0, image.GetWidth(), image.GetHeight()); // 원본그대로
+
+	EndPaint(hwnd, &ps);
+}
+
+
+// * 윈도우 프로시져 함수 만들기 - callback
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
 	switch(message)
 	{
+		case WM_PAINT:
+		{
+			OnPaint(hwnd);
+		}
+		break;
 		case WM_LBUTTONDOWN:
 		{
 			//int x = (WORD)lparam;
