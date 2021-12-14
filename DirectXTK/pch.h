8cff7204425042ea7df76c56267297d17cadf901
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-14 16:54:50 +0900

    라이브러리사용시작_툴킷_pch헤더

---
+++ DirectXTK/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+// 가정용 windows
+// 기업 서버용 Wndows NT
+
+// Windows7		0x0601
+// Window8		0x0602
+// Window8.1	0x0603
+// Window10		0x0A00
+#include <winsdkver.h>
+#define _WIN32_WINNT 0x0A00	// OS 최대버전 설정(windows10 전용)
+
+#include <winsdkver.h>
+
