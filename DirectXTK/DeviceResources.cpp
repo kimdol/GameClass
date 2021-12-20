9760ae6c5c695f1785dad139944c2bf9210ca89e
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-20 16:57:52 +0900

    DeviceResources함수선언_NoSRGB

--- DirectXTK/DeviceResources.cpp
+++ DirectXTK/DeviceResources.cpp
@@ -1,5 +1,93 @@
 #include "pch.h"
 #include "DeviceResources.h"
 
+using namespace DirectX;
 using namespace DX;
+using Microsoft::WRL::ComPtr;
 
+#ifdef __clang__	// 컴파일러
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+#pragma clang diagnostic ignored "-Wswitch-enum"
+#endif
+
+#pragma warning(disable : 4061)
+
+namespace
+{
+#if defined(_DEBUG)
+	inline bool SdkLayersAvailable() noexcept
+	{
+		HRESULT hr = D3D11CreateDevice(
+			nullptr,
+			D3D_DRIVER_TYPE_NULL,
+			nullptr,
+			D3D11_CREATE_DEVICE_DEBUG,
+			nullptr,
+			0,
+			D3D11_SDK_VERSION,
+			nullptr,
+			nullptr,
+			nullptr
+		);
+
+		return SUCCEEDED(hr);
+	}
+#endif
+
+	inline DXGI_FORMAT NoSRGB(DXGI_FORMAT fmt) noexcept
+	{
+		switch(fmt)
+		{
+			case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:
+				return DXGI_FORMAT_R8G8B8A8_UNORM;
+			case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:
+				return DXGI_FORMAT_B8G8R8A8_UNORM;
+			case DXGI_FORMAT_B8G8R8X8_UNORM_SRGB:
+				return DXGI_FORMAT_B8G8R8X8_UNORM;
+			default:
+				return fmt;
+		}
+	}
+}
+
+void DeviceResources::CreateFactory()
+{
+	
+}
+
+void DeviceResources::GetHardwareAdapter(IDXGIAdapter1** ppAdapter)
+{
+}
+
+void DeviceResources::UpdateColorSpace()
+{
+}
+
+DeviceResources::DeviceResources()
+{
+}
+
+void DeviceResources::CreateDeviceResources()
+{
+}
+
+void DeviceResources::CreateWindowSizeDependentResources()
+{
+}
+
+void DeviceResources::SetWindow(HWND window, int width, int height) noexcept
+{
+}
+
+bool DeviceResources::WindowSizeChanged(int width, int height)
+{
+	return false;
+}
+
+void DeviceResources::HandleDeviceLost()
+{
+}
+
+void DeviceResources::Present()
+{
+}
--- DirectXTK/DeviceResources.h
+++ DirectXTK/DeviceResources.h
@@ -36,9 +36,72 @@ namespace DX
 		RECT					m_outputSize;
 
 		DXGI_COLOR_SPACE_TYPE	m_colorSpace;
-		unsigned				m_options;
+		unsigned int			m_options;	// 32개만듬
 
 		IDeviceNotify*			m_deviceNotify;
+
+	public:
+		static const unsigned int c_FlipPresent = 0x01;
+		static const unsigned int c_AllowTearing = 0x2;
+		static const unsigned int c_EnableHDR = 0x04;
+
+	private:
+		void CreateFactory();
+		void GetHardwareAdapter(IDXGIAdapter1** ppAdapter);
+		void UpdateColorSpace();
+
+	public:
+		DeviceResources();
+		~DeviceResources() = default;	// 숨기는 이유는 스마트 포인터를 쓰겠다는 뜻
+	
+		DeviceResources(DeviceResources&&) = default;	// 이동생성자
+		DeviceResources& operator = (DeviceResources&&) = default; // 이동대입연산자
+	
+		DeviceResources(const DeviceResources&) = delete;
+		DeviceResources& operator = (const DeviceResources&) = delete;
+
+	public:
+		void CreateDeviceResources();
+		void CreateWindowSizeDependentResources();
+		void SetWindow(HWND window, int width, int height) noexcept;
+		bool WindowSizeChanged(int width, int height);
+		void HandleDeviceLost();
+		void RegisterDeviceNotify(IDeviceNotify* deviceNotify) noexcept
+		{
+			m_deviceNotify = deviceNotify;
+		}
+		void Present(); // 그리기
+
+	public:
+		RECT GetOutputSize() const noexcept { return m_outputSize; }
+		auto GetD3DDevice() const noexcept { return m_d3dDevice.Get(); }
+		auto GetD3DDeviceContext() const noexcept { return m_d3dContext.Get(); }
+		auto GetDXGIFactory() const noexcept { return m_dxgiFactory.Get(); }
+		auto GetWindow() const noexcept { return m_window; }
+		auto GetDeviceFeatureLevel() const noexcept { return m_d3dFeatureLevel; }
+		auto GetRenderTarget() const noexcept { return m_renderTarget.Get(); }
+		auto GetDepthStencil() const noexcept { return m_depthStencil.Get(); }
+		auto GetRenderTargetView() const noexcept { return m_d3dRenderTargetView.Get(); }
+		auto GetDepthStencilView() const noexcept { return m_d3dDepthStencilView.Get(); }
+		auto GetBackBufferFormat() const noexcept { return m_backBufferFormat; }
+		auto GetDepthBufferFormat() const noexcept { return m_depthBufferFormat; }
+		auto GetScreenViewport() const noexcept { return m_screenViewport; }
+		auto GetColorSpace() const noexcept { return m_colorSpace; }
+		auto GetDeviceOptions() const noexcept { return m_options; }
+
+	public:
+		void PIXBeginEvent(_In_z_ const wchar_t* name)
+		{
+			m_d3dAnnotation->BeginEvent(name);
+		}
+		void PIXEndEvent()
+		{
+			m_d3dAnnotation->EndEvent();
+		}
+		void PIXSetMarker(_In_z_ const wchar_t* name)
+		{
+			m_d3dAnnotation->SetMarker(name);
+		}
 	};
 }
 
