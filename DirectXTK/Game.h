f7f9a8e0503f31c5ddb402077ce4f22714cc7de4
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-23 16:53:30 +0900

    DirectTK사용법_region_고성능그래픽카드변수덮기

---
+++ DirectXTK/Game.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+class Game final :
+    public DX::IDeviceNotify
+{
+private:
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources; // has a관계
+    DX::StepTimer                           m_timer;
+    std::unique_ptr<DirectX::Keyboard>      m_keyboard;
+    std::unique_ptr<DirectX::Mouse>         m_mouse;
+
+public:
+    Game() noexcept(false);
+    ~Game();
+    // 메모리를 전부다 스마트포인터로 쓴다는 뜻
+    Game(Game&&) = default;
+    Game& operator=(Game&&) = default;
+
+    Game(Game const&) = delete;
+    Game& operator=(Game const&) = delete;
+
+    void Initialize(HWND window, int width, int height);
+
+    void Tick();
+
+    void OnDeviceLost() override;
+    void OnDeviceRestored() override;
+
+    void OnActivated();
+    void OnDeActivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowMoved();
+    void OnWindowSizeChanged(int width, int& height);
+
+    void GetDefaultSize(int& width, int& height) const noexcept;
+
+private:
+    void Update(DX::StepTimer const& timer);
+    void Render();
+
+    void Clear();
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+
+};
+
--- DirectXTK/WinMain.cpp
+++ DirectXTK/WinMain.cpp
@@ -1,10 +1,44 @@
 #include "pch.h"
+#include "Game.h"
+
+using namespace DirectX;
+
+namespace
+{
+	std::unique_ptr<Game> g_game;
+}
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+// c언어로 컴파일되게끔 함
+extern "C"
+{
+	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd)
 {
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (!XMVerifyCPUSupport())
+	{
+		return 1;
+	}
+
+	HRESULT hr = CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
+	if (FAILED(hr))
+	{
+		return 1;
+	}
+
+	g_game = std::make_unique<Game>();
+
+
+	CoUninitialize();
 
 	return 0;
 }
