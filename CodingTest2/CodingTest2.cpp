62d321a108872d068160de39c201ddab2f12c0f7
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-29 16:27:56 +0900

    DynamicProgramming

---
+++ CodingTest2/CodingTest2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+
+using fibo_history = std::map<int, int64_t>;
+
+int64_t Fibonacci(int n, fibo_history& h)
+{
+    if (h.count(n) == 1)
+    {
+        return h[n];
+    }
+
+    // base case
+    if (n <= 2)
+    {
+        return 1;
+    }
+
+    // recursive case
+    h[n] = Fibonacci(n - 1, h) + Fibonacci(n - 2, h);
+    return h[n];
+}
+
+int main()
+{
+    fibo_history h;
+
+    std::cout << Fibonacci(50, h) << std::endl;
+}
